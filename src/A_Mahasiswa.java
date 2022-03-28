5339b01815432520d74255bc39a3dfcaf6313dd0
Maysintasamosir <Maysintasamosir@gmail.com>
2022-03-28 16:15:04 +0700

    percobaan 1 mengakses anggota kelas9member class)

--- src/A_Mahasiswa.java
+++ src/A_Mahasiswa.java
@@ -1,14 +1,15 @@
-package konsepoop;
 public class A_Mahasiswa {
-    public int nrp;
-    public String nama;
-    public void info() {
-        System.out.println("lni siswa RPL Polbeng");
-    }
-        public static void main(String argsO) {
-            konsepoop.A_Mahasiswa it = new konsepoop.A_Mahasiswa();
+        public int nrp;
+        public String nama;
+        public void info() {
+            System.out.println("ini siswa RPL Polbeng");
+        }
+        public static void main(String args[]) {
+            A_Mahasiswa it = new A_Mahasiswa();
             it.nrp = 5;
             it.nama = "Andi";
             it.info();
         }
-        }
+    }
+
+
