cbd0a3269e30d84c5a742ae605ffbb9ec4ff31d0
Maysintasamosir <Maysintasamosir@gmail.com>
2022-03-28 14:45:09 +0700

    percoban 3 membuat class dan objek dengan dua file kod sumber yg berbeda

---
+++ src/C_Orang.java
@@ -0,0 +1,21 @@
+package konsepoop;
+public class C_Orang {
+    String nama;
+    int umur;
+    String warnaRambut;
+    String warnaKulit;
+    int tinggiBadan;
+    int beratBadan;
+
+    public C_Orang() {
+    }
+    public C_Orang(String nama) {
+        this.nama = nama;
+    }
+    public void menangis() {
+        System.out.println("hu..hu..");
+    }
+    public void tertawa() {
+        System.out.println("Ha....Ha....");
+    }
+}
